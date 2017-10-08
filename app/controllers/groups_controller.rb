c4259789552bcbe74e26a62aae571fd085f0f548
Po-Han <gn00499901@gmail.com>
2017-10-08 22:41:06 +0800

    implement my post

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,7 +2,7 @@ class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
-    @groups = current_user.participated_groups
+    @groups = Group.all
   end
   def new
   	@group = Group.new
